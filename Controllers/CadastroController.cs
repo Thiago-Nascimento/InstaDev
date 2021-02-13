8c125b6f21186d3fedc0dac25a112cb4c392e4e0
Thiago-Nascimento <thiago.r.nascimento@hotmail.com>
2021-02-13 12:43:44 -0300

    View Cadastro criada

--- Controllers/CadastroController.cs
+++ Controllers/CadastroController.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace InstaDev.Controllers
 {
+    [Route("Cadastro")]
     public class CadastroController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+        
     }
 }
